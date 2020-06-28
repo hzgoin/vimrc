3d48f29800d12dca78d7bd0ba6dd300562c1f22c
wangmeng8 <wangmeng8@staff.sina.com.cn>
2020-06-28 20:48:09 +0800

    feat:add cmd-line flag -h --help --all --list

--- update_my_plugins.py
+++ update_my_plugins.py
@@ -10,13 +10,14 @@ import zipfile
 import shutil
 import tempfile
 import requests
+import sys
 
 from os import path
 
 # --- Globals ----------------------------------------------
 PLUGINS = """
 vim-go https://github.com/fatih/vim-go
-vim-ansible-yaml https://github.com/chase/vim-ansible-yaml
+ansible-vim https://github.com/pearofducks/ansible-vim
 """.strip()
 
 GITHUB_ZIP = "%s/archive/master.zip"
@@ -57,13 +58,42 @@ def update(plugin):
 
 
 if __name__ == "__main__":
+    doc = """usage: python3 %s [-h|--help] [--all] [--list] [plugin]...
+
+-h --help   print this help doc
+--all       update all plugins
+--list      list supported plugins
+[plugin]    update the plugin specified by multi [plugin]
+    """ % sys.argv[0]
+    if len(sys.argv) == 1 or sys.argv[1] == "-h" or sys.argv[1] == "--help":
+        print (doc)
+        sys.exit(0)
+
+    plugins = PLUGINS.splitlines()
+    if sys.argv[1] == "--list":
+            for plugin in plugins:
+                print (plugin)
+            sys.exit(0)
+
     temp_directory = tempfile.mkdtemp()
 
     try:
-        if futures:
-            with futures.ThreadPoolExecutor(16) as executor:
-                executor.map(update, PLUGINS.splitlines())
+
+        if sys.argv[1] == "--all":
+            if futures:
+                with futures.ThreadPoolExecutor(16) as executor:
+                    executor.map(update, plugins)
+            else:
+                [update(x) for x in plugins]
         else:
-            [update(x) for x in PLUGINS.splitlines()]
+            plugin_dict = {}
+            for plugin in plugins:
+                name, _ = plugin.split(" ")
+                plugin_dict[name] = plugin
+
+            for x in sys.argv[1:]:
+                if x in plugin_dict:
+                    update(plugin_dict[x])
+
     finally:
         shutil.rmtree(temp_directory)
